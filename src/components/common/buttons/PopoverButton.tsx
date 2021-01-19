6ac58a6de2a42a466ff5215ee64eb370ab5e59e8
Bonkook Koo <justinaus2k@gmail.com>
2021-01-19 13:57:00 +0900

    update : popover button props onclick

--- src/components/common/buttons/PopoverButton.tsx
+++ src/components/common/buttons/PopoverButton.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
-export default function PopoverButton() {
+interface Props {
+  onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+export default function PopoverButton({ onClick }: Props) {
   return (
     <div>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={(e) => onClick(e)}>
         Hello World
       </Button>
     </div>
