68db0448e86345a328dad3939755b9d8c3c1ffb7
Bonkook Koo <justinaus2k@gmail.com>
2021-01-19 23:12:26 +0900

    update : posts page

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,4 +1,19 @@
-import React from 'react';
+import { Observer } from 'mobx-react';
+import React, { useEffect } from 'react';
+import BasicTable from '../components/common/tables/BasicTable';
+import ApiPath from '../services/ApiPath';
+import TableListStore from '../stores/TableListStore';
+
 export default function Posts() {
-  return <div>List Page</div>;
+  useEffect(() => {
+    const path = `${ApiPath.posts}?_page=2&_limit=10`;
+
+    TableListStore.getData(path);
+  }, []);
+  return (
+    <div>
+      List Page
+      <Observer>{() => <BasicTable rows={TableListStore.list} />}</Observer>
+    </div>
+  );
 }
