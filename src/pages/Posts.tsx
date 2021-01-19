847e817a44b11badc4d7616d37759dde1ba28ffd
Bonkook Koo <justinaus2k@gmail.com>
2021-01-19 14:01:00 +0900

    update : rename

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,11 +1,4 @@
 import React from 'react';
-import PopoverButton from '../components/common/buttons/PopoverButton';
-
-export default function TestList() {
-  return (
-    <div>
-      List Page
-      <PopoverButton />
-    </div>
-  );
+export default function Posts() {
+  return <div>List Page</div>;
 }
